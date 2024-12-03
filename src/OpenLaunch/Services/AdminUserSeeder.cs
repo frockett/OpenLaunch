849c23bfc52f30a15f9323ce96a536da4f90dbe1
frockett <crockett.d.ford@gmail.com>
2024-12-03 20:35:30 +0800

    added sqlite files to gitignore correctly

--- src/OpenLaunch/Services/AdminUserSeeder.cs
+++ src/OpenLaunch/Services/AdminUserSeeder.cs
@@ -19,25 +19,16 @@ public class AdminUserSeeder
         var adminUsername = Environment.GetEnvironmentVariable("ADMIN_USERNAME") ?? "admin";
         var adminPassword = Environment.GetEnvironmentVariable("ADMIN_PASSWORD") ?? "password";
         
-        Console.WriteLine("Seeding admin user");
-        
         if (!await _roleManager.RoleExistsAsync("Admin"))
         {
-            Console.WriteLine("NO ROLE EXISTS, MAKE IT");
             await _roleManager.CreateAsync(new IdentityRole("Admin"));
-        }
-        else
-        {
-            Console.WriteLine("ADMIN ROLE ALREADY EXISTS");
         }
         
         var adminUser = await _userManager.FindByNameAsync(adminUsername);
         if (adminUser == null)
         {
-            Console.WriteLine("THE USER DOESN'T EXIST, MAKE IT");
             adminUser = new AppUser { UserName = adminUsername, Email = $"{adminUsername}@example.com" , EmailConfirmed = true};
             var result = await _userManager.CreateAsync(adminUser, adminPassword);
-            Console.WriteLine($"RESULT: {result.ToString()}");
             if (result.Succeeded)
             {
                 await _userManager.AddToRoleAsync(adminUser, "Admin");
